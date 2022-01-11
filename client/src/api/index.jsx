608f39ad79f6cbb4d698d20a8f08934e9876a9f8
youhavetosleep <qksekf4@gmail.com>
2022-01-11 21:35:42 +0900

    로그인 리팩토링 셋팅

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const url = process.env.REACT_APP_API_URL || 'http://localhost:80'
+console.log(url)
 const instance = axios.create({
   baseURL: url
 })
